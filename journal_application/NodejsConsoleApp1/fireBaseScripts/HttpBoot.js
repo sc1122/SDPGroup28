bc90cc769a78c76e354cfe30b286935c58dee8bc
Nick Batson <nickbatson174@gmail.com>
2017-08-17 17:43:23 +1000

    Initial Commit

---
+++ journal_application/NodejsConsoleApp1/fireBaseScripts/HttpBoot.js
@@ -0,0 +1,16 @@
+﻿var http = require('http');
+
+exports.bootTestServer = function (categories) {
+
+    http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+        var arrayLength = categories.length;
+        for (var i = 0; i < arrayLength; i++) {
+            res.write(categories[i] + "\n")
+            //Do something
+        }
+        res.end('Hello World!');
+    }).listen(8080);
+
+}
---
+++ journal_application/NodejsConsoleApp1/fireBaseScripts/fireBasics.js
@@ -0,0 +1,46 @@
+﻿var httpBoot = require('./HttpBoot');
+
+exports.upload_user = function (admin, user_name, user_text, date_p) {
+    var ref = admin.database().ref('user_db');
+    var userRef = ref.child('user/' + user_name);
+    userRef.push({
+        name: user_name,
+        Text: user_text,
+        //date_time: date_p
+    });
+    console.log('User added: ' + user_name);
+};
+
+exports.myDateTime = function () {
+    return Date();
+};
+
+exports.search_user = function (admin, startServer) {
+    console.log("Delivering users")
+    var array = new Array();
+    var count = 0;
+    var ref = admin.database().ref("user_db/user");
+    var user;
+    ref.on("value", function (snapshot) {
+        console.log(snapshot.val());
+        
+        snapshot.forEach(function (data) {
+            console.log("*");
+            user = data;
+            console.log(data.key);
+            array[count] = data.key;
+            count += 1;
+        });
+        startServer(array);
+    });
+
+}
+
+exports.startServer = function (cat) {
+    httpBoot.bootTestServer(cat);
+    console.log(cat[0]);
+    console.log("Async finished");
+}
+
+
+
